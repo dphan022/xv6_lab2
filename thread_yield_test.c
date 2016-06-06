20bcd405ce6e177df250ccce8d5a6fb5d435e62a
David Phan <dphan015@ucr.edu>
2016-06-05 17:07:09 -0700

    thread yield testcase update

--- thread_yield_test.c
+++ thread_yield_test.c
@@ -1,46 +1,23 @@
-#include "param.h"
 #include "types.h"
-#include "stat.h"
 #include "user.h"
-#include "fs.h"
-#include "fcntl.h"
-#include "syscall.h"
-#include "traps.h"
-#include "memlayout.h"
-#include "semaphore.h"
 
 void thread_test(void* arg);
 
 int main(void) {
-    void* t1 = thread_create(thread_test, (void*) 0 );
-    if(t1 == 0){
-        printf(1, "t1 : thread_create failed \n");
-        exit();
-    }
+    int i;
+    void* tid;
 
-    void* t2 = thread_create(thread_test, (void*) 0 );
-    if(t2 == 0){
-        printf(1, "t2 : thread_create failed \n");
-        exit();
-    }
-
-    void* t3 = thread_create(thread_test, (void*) 0 );
-    if(t3 == 0){
-        printf(1, "t3 : thread_create failed \n");
-        exit();
-    }
-
-    void* t4 = thread_create(thread_test, (void*) 0 );
-    if(t4 == 0){
-        printf(1, "t4 : thread_create failed \n");
-        exit();
+    for(i = 0; i < 10; i++){
+        tid = thread_create(thread_test, (void*)&i);
+        if(tid == 0){
+            printf(1, "thread_create # %d failed \n", tid);
+        }
     }
 
     while(wait() > 0);
     printf(1,"All threads yielded properly.\n");
     exit();
     return 0;
-
 }
 
 
